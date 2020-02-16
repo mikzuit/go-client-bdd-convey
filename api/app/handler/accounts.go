776ddff2005a7049272789a777f5cacfafd2df8f
mikzuit <mr_social1@blueevo.com>
2020-02-16 13:57:18 +0100

    Create accounts.go

---
+++ api/app/handler/accounts.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/mikzuit/api/app/model"
+	"github.com/jinzhu/gorm"
+)
+
+// Handler for Create accounts in POST method
+func CreateAccount(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+
+	// define and assing payload model
+	record := model.Payload{}
+
+	// decode payload
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&record); err != nil {
+		respondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := db.Save(&record.Data).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return 
+	}
+	respondJSON(w, http.StatusCreated, record)
+}
