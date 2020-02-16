56f272216e4f8ab98a750cafc2b15a250dfa6bfe
mikzuit <mr_social1@blueevo.com>
2020-02-16 14:14:00 +0100

    Update accounts.go

--- api/app/handler/accounts.go
+++ api/app/handler/accounts.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"github.com/mikzuit/api/app/model"
 	"github.com/jinzhu/gorm"
+	"github.com/gorilla/mux"
 )
 
 // Handler for Create accounts in POST method
@@ -30,5 +31,43 @@ func CreateAccount(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 }
 
 func FetchAccount(db *gorm.DB, w http.ResponseWriter, r *http.Request){
+	account := model.Account{}
 
+	vars := mux.Vars(r)
+	accountid := vars["accountid"]
+		if err := db.First(&account, "aid = ?", accountid).Error; err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+	}
+
+	respondJSON(w, http.StatusOK, account)
+}
+
+// Handler for DELETE accounts in POST method
+func DeleteAccount(db *gorm.DB, w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	accountid := vars["accountid"]
+
+	// get existent account or 404
+	account := getAccountOr404(db, accountid, w, r)
+	if account == nil {
+		return
+	}
+
+	// Unscoped delete is a removed model, only delete just set the time deleted
+	// this last one might cause unreliable testing
+	if err := db.Unscoped().Delete(&account).Error; err != nil {
+		respondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondJSON(w, http.StatusNoContent, nil)
+}
+
+// util function to retrieve models or 404
+func getAccountOr404(db *gorm.DB, accountid string, w http.ResponseWriter, r *http.Request) *model.Account {
+	account := model.Account{}
+	if err := db.First(&account, model.Account{Aid: accountid}).Error; err != nil {
+		respondError(w, http.StatusNotFound, err.Error())
+		return nil
+	}
+	return &account
 }
