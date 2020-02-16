6c155834c126fb6c1436046eea124562c1fa73b6
mikzuit <mr_social1@blueevo.com>
2020-02-16 14:02:15 +0100

    Update app.go

--- api/app/app.go
+++ api/app/app.go
@@ -43,7 +43,13 @@ func(a *App) Initialize(config *config.Config) {
 }
 
 func (a *App) setRouters(){
+	a.Get("/v1/organisation/accounts/{accountid}", a.FetchAccount)
 	a.Post("/v1/organisation/accounts", a.CreateAccount)
+}
+
+// Wrap the router for GET method
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("GET")
 }
 
 // Wrap the router for POST method
@@ -51,9 +57,19 @@ func (a *App) Post(path string, f func(w http.ResponseWriter, r *http.Request)) 
 	a.Router.HandleFunc(path, f).Methods("POST")
 }
 
+// Wrap the router for DELETE method
+func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("DELETE")
+}
+
 // indicated handler for create account
 func (a *App) CreateAccount(w http.ResponseWriter, r *http.Request) {
 	handler.CreateAccount(a.DB, w, r)
+}
+
+// indicated handler for fetch account
+func (a *App) FetchAccount(w http.ResponseWriter, r *http.Request) {
+	handler.FetchAccount(a.DB, w, r)
 }
 
 func (a *App) Run(host string) {
